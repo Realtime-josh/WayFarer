1b8b4fbb30718f822577dea2880513587fb9446b
JoshTouche <franksonjoshua@gmail.com>
2019-06-30 12:16:27 +0100

    models setup

--- server/models/test-tables-setup.sql
+++ server/models/test-tables-setup.sql
@@ -1,13 +1,13 @@
 \connect wayfarer
-CREATE TABLE IF NOT EXISTS user
+CREATE TABLE IF NOT EXISTS USER
  (
-    user_id serial PRIMARY KEY,
-    user_email text UNIQUE NOT NULL, 
-    user_email text NOT NULL,
-    first_name text NOT NULL,
-    last_name text NOT NULL,
-    password text NOT NULL,
-    is_admin boolean DEFAULT false,
+   user_id serial PRIMARY KEY,
+   user_email text UNIQUE NOT NULL, 
+   user_email text NOT NULL,
+   first_name text NOT NULL,
+   last_name text NOT NULL,
+   password text NOT NULL,
+   is_admin boolean DEFAULT false,
 );
 
 
