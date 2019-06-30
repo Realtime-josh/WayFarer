fa031c5790452508a3fb68b0998359569dc12884
JoshTouche <franksonjoshua@gmail.com>
2019-06-30 08:58:44 +0100

    models setup

--- server/models/test-tables-setup.sql
+++ server/models/test-tables-setup.sql
@@ -1,7 +1,5 @@
 \connect wayfarer
-DROP TABLE  IF EXISTS  user CASCADE;
-CREATE TABLE IF NOT EXISTS user
- (
+CREATE TABLE IF NOT EXISTS user(
     user_id serial PRIMARY KEY,
     user_email text UNIQUE NOT NULL, 
     user_email text NOT NULL,
@@ -9,6 +7,6 @@ CREATE TABLE IF NOT EXISTS user
     last_name text NOT NULL,
     password text NOT NULL,
     is_admin boolean DEFAULT false,
- );
+);
 
 
